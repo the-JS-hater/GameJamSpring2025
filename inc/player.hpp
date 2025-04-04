a4f8f79e90e5f2e320e0ae0f74345cf6e1e1cb14
Alexander Järnström <alexander.jarnstrom@gmail.com>
2025-04-04 22:07:12 +0200

    -Fixed merge

--- inc/player.hpp
+++ inc/player.hpp
@@ -1,11 +1,25 @@
 #include "ecs.hpp"
 #include "entityType.hpp"
+#include <cstddef>
+
+enum Hand {
+  left,
+  right
+};
 
 struct Player
 {
-	Entity id;
+	Entity body;
+	Entity hand_l;
+	Entity hand_r;
+
+  size_t max_v;
+  size_t max_d;
+
+  enum Hand active_hand;
 
   void input(ECS& ecs);
+  void update(ECS& ecs);
 };
 
 Player init_player(ECS &ecs);
--- src/player.cpp
+++ src/player.cpp
@@ -1,33 +1,153 @@
 #include "../inc/player.hpp"
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <ostream>
+#include <raylib.h>
+#include <iostream>
 
-void Player::input(ECS &ecs) {
-  	if (IsKeyDown(KEY_W)) (*ecs.velocities.getComponent(this->id)).vy -= 1.0f;
-		if (IsKeyDown(KEY_A)) (*ecs.velocities.getComponent(this->id)).vx -= 1.0f;
-		if (IsKeyDown(KEY_S)) (*ecs.velocities.getComponent(this->id)).vy += 1.0f;
-		if (IsKeyDown(KEY_D)) (*ecs.velocities.getComponent(this->id)).vx += 1.0f;
+void Player::input(ECS &ecs)
+{
+  (*ecs.velocities.getComponent(this->hand_l)).vx -= (*ecs.velocities.getComponent(this->body)).vx;
+  (*ecs.velocities.getComponent(this->hand_l)).vy -= (*ecs.velocities.getComponent(this->body)).vy;
+  (*ecs.velocities.getComponent(this->hand_r)).vx -= (*ecs.velocities.getComponent(this->body)).vx;
+  (*ecs.velocities.getComponent(this->hand_r)).vy -= (*ecs.velocities.getComponent(this->body)).vy;
+
+  (*ecs.velocities.getComponent(this->body)).vy = 0.0f;
+  (*ecs.velocities.getComponent(this->body)).vx = 0.0f;
+
+
+  if (IsKeyDown(KEY_SPACE)) switch (this->active_hand)
+  {
+    case Hand::left:
+      this->active_hand = Hand::right;
+      break;
+    case Hand::right:
+      this->active_hand = Hand::left;
+      break;
+  };
+
+  if (IsKeyDown(KEY_UP)) (*ecs.velocities.getComponent(this->body)).vy = -2.0f;
+  if (IsKeyDown(KEY_LEFT)) (*ecs.velocities.getComponent(this->body)).vx = -2.0f;
+  if (IsKeyDown(KEY_DOWN)) (*ecs.velocities.getComponent(this->body)).vy = 2.0f;
+  if (IsKeyDown(KEY_RIGHT)) (*ecs.velocities.getComponent(this->body)).vx = 2.0f;
+
+  (*ecs.velocities.getComponent(this->hand_l)).vx += (*ecs.velocities.getComponent(this->body)).vx;
+  (*ecs.velocities.getComponent(this->hand_l)).vy += (*ecs.velocities.getComponent(this->body)).vy;
+  (*ecs.velocities.getComponent(this->hand_r)).vx += (*ecs.velocities.getComponent(this->body)).vx;
+  (*ecs.velocities.getComponent(this->hand_r)).vy += (*ecs.velocities.getComponent(this->body)).vy;
+
+
+  
+  switch (this->active_hand)
+  {
+    case Hand::left: 
+      if (IsKeyDown(KEY_W)) (*ecs.velocities.getComponent(this->hand_l)).vy -= 1.0f;
+      if (IsKeyDown(KEY_A)) (*ecs.velocities.getComponent(this->hand_l)).vx -= 1.0f;
+      if (IsKeyDown(KEY_S)) (*ecs.velocities.getComponent(this->hand_l)).vy += 1.0f;
+      if (IsKeyDown(KEY_D)) (*ecs.velocities.getComponent(this->hand_l)).vx += 1.0f;
+      break;
+    case Hand::right: 
+      if (IsKeyDown(KEY_W)) (*ecs.velocities.getComponent(this->hand_r)).vy -= 1.0f;
+      if (IsKeyDown(KEY_A)) (*ecs.velocities.getComponent(this->hand_r)).vx -= 1.0f;
+      if (IsKeyDown(KEY_S)) (*ecs.velocities.getComponent(this->hand_r)).vy += 1.0f;
+      if (IsKeyDown(KEY_D)) (*ecs.velocities.getComponent(this->hand_r)).vx += 1.0f;
+      break;
+  }
+}
+
+void Player::update(ECS& ecs)
+{
+  Position b_pos = (*ecs.positions.getComponent(this->body));
+  Position h_l_pos = (*ecs.positions.getComponent(this->hand_l));
+  Position h_r_pos = (*ecs.positions.getComponent(this->hand_r));
+
+  float h_l_dis = (float) sqrt(pow(h_l_pos.x - b_pos.x, 2) + pow(h_l_pos.y - b_pos.y, 2)); 
+  float h_r_dis = (float) sqrt(pow(h_r_pos.x - b_pos.x, 2) + pow(h_r_pos.y - b_pos.y, 2)); 
+
+  if (h_l_dis > this->max_d) // Left hand too far away
+  {
+    // std::cout << "Left hand angle: " << atan2(h_l_pos.y - b_pos.y, h_l_pos.x - b_pos.x) << std::endl;
+
+    // Temporary dont ask
+    (*ecs.velocities.getComponent(this->hand_l)).vx = 0.0;
+    (*ecs.velocities.getComponent(this->hand_l)).vy = 0.0;
+  }
+
+  if (h_r_dis > this->max_d) // Right hand too far away
+  {
+    // std::cout << "Right hand angle: " << atan2(h_r_pos.y - b_pos.y, h_r_pos.x - b_pos.x) << std::endl;
+
+    // Temporary dont ask
+    (*ecs.velocities.getComponent(this->hand_r)).vx = 0.0;
+    (*ecs.velocities.getComponent(this->hand_r)).vy = 0.0;
+  }
 }
 
 Player init_player(ECS& ecs) {
-	Texture2D playerTex = LoadTexture("resources/sprites/Spam.png");
-	Entity playerId = ecs.createEntity();
+	Texture2D body_tex = LoadTexture("resources/sprites/Spam.png");
+	Texture2D hand_l_tex = LoadTexture("resources/sprites/Spam.png");
+	Texture2D hand_r_tex = LoadTexture("resources/sprites/Spam.png");
+	Entity body = ecs.createEntity();
+	Entity hand_l = ecs.createEntity();
+	Entity hand_r = ecs.createEntity();
 
-  Position pos = { 
-		static_cast<float>(rand() % GetScreenWidth()), 
-		static_cast<float>(rand() % GetScreenHeight()) 
-	};
+  // Body
+  Position b_pos = { 
+    static_cast<float>(rand() % GetScreenWidth()), 
+    static_cast<float>(rand() % GetScreenHeight()) 
+  };
 
   Velocity vel = { 0.0f, 0.0f };
   Dimension dim = { 32.0f, 32.0f };
+  Collider collider;
+  collider.update(b_pos, dim);
 
-	Collider collider;
-  collider.update(pos, dim);
-  
-  ecs.positions.insert(playerId, pos);
-  ecs.velocities.insert(playerId, vel);
-  ecs.dimensions.insert(playerId, dim);
-  ecs.colliders.insert(playerId, collider);
-	ecs.sprites.insert(playerId, Sprite{playerTex});
-	
-  return Player{ playerId };
+  ecs.positions.insert(body, b_pos);
+  ecs.velocities.insert(body, vel);
+  ecs.dimensions.insert(body, dim);
+  ecs.colliders.insert(body, collider);
+  ecs.sprites.insert(body, Sprite{body_tex});
+
+  // Left hand
+  {
+    Position pos = { 
+      static_cast<float>(b_pos.x + rand() % 10), 
+      static_cast<float>(b_pos.y + rand() % 10) 
+    };
+
+    Velocity vel = { 0.0f, 0.0f };
+    Dimension dim = { 32.0f, 32.0f };
+    Collider collider;
+    collider.update(pos, dim);
+
+    ecs.positions.insert(hand_l, pos);
+    ecs.velocities.insert(hand_l, vel);
+    ecs.dimensions.insert(hand_l, dim);
+    ecs.colliders.insert(hand_l, collider);
+    ecs.sprites.insert(hand_l, Sprite{hand_l_tex});
+  }
+
+
+  // Right hand
+  {
+    Position pos = { 
+      static_cast<float>(b_pos.x + rand() % 10), 
+      static_cast<float>(b_pos.y + rand() % 10) 
+    };
+
+    Velocity vel = { 0.0f, 0.0f };
+    Dimension dim = { 32.0f, 32.0f };
+    Collider collider;
+    collider.update(pos, dim);
+
+    ecs.positions.insert(hand_r, pos);
+    ecs.velocities.insert(hand_r, vel);
+    ecs.dimensions.insert(hand_r, dim);
+    ecs.colliders.insert(hand_r, collider);
+    ecs.sprites.insert(hand_r, Sprite{hand_r_tex});
+  }
+
+  return Player{ body, hand_l, hand_r, 2, 200, Hand::left };
 }
 
