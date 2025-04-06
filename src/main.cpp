ad3da3ae4a2b69d42ae09e27d9620ed3eb70257e
Morgan <mnordberg01@gmail.com>
2025-04-06 11:05:01 +0200

    crowd grafixTM

--- src/main.cpp
+++ src/main.cpp
@@ -27,18 +27,18 @@ int main()
   Position pos_1 {static_cast<float>(WINDOW_W)/2.0f, static_cast<float>(WINDOW_H)/4.0f,};
   Position pos_2 {pos_1.x, pos_1.y * 3};
 
-	Texture2D gloveTex = LoadTexture("resources/sprites/Glove.png");
-	Texture2D ratTex = LoadTexture("resources/sprites/Rat.png");
-	Texture2D gloveTex2 = LoadTexture("resources/sprites/Glove2.png");
-	Texture2D ratTex2 = LoadTexture("resources/sprites/Rat2.png");
-	Texture2D crowd1 = LoadTexture("resources/sprites/Crowd1.png");
-	Texture2D crowd2 = LoadTexture("resources/sprites/Crowd2.png");
-
+	Texture2D gloveTex 		= LoadTexture("resources/sprites/Glove.png");
+	Texture2D ratTex 			= LoadTexture("resources/sprites/Rat.png");
+	Texture2D gloveTex2 	= LoadTexture("resources/sprites/Glove2.png");
+	Texture2D ratTex2 		= LoadTexture("resources/sprites/Rat2.png");
+	Texture2D crowd 			= LoadTexture("resources/sprites/animCrowd.png"); 
+	Texture2D crowd2 			= LoadTexture("resources/sprites/animCrowd2.png"); 
+	
 	Player player_1 = init_player(ecs, pos_1, gloveTex, ratTex);
 	Player player_2 = init_player(ecs, pos_2, gloveTex2, ratTex2);
   VictoryScreen screen(WINDOW_W, WINDOW_H);
   
-	ElectricHex hex(20.0f, 15.0f, 5.0f, 2.0f, 50.0f, 30.0f); 
+	ElectricHex hex(20.0f, 15.0f, 8.0f, 2.0f, 47.0f, 30.0f); 
 
   Rectangle worldBounds = {
     0, 0, 
@@ -88,22 +88,41 @@ int main()
     player_1.draw_score(1, 20);
     player_2.draw_score(2, 50);
 		ecs.renderEntities();
-			
-		DrawTextureEx(
-			crowd1, 
-			{0.0f, GetScreenHeight() / 2 - crowd1.height}, 
-			0.0f, 
-			2.0f, 
+		
+		Rectangle source {
+			0.0f,
+			(crowd.height / 3.0f) * (rand()%3),
+			crowd.width,
+			crowd.height / 3.0f 
+		};
+
+		DrawTexturePro(
+			crowd,
+			source,
+			{
+				10.0f, 
+				GetScreenHeight() / 3, 
+				2.0f * crowd.width, 
+				2.0f * crowd.height / 3.0f
+			}, 
+			{0.0f, 0.0f},
+			0.0f,
 			WHITE
 		);
-		DrawTextureEx(
-			crowd2, 
+
+		source.y = (crowd.height / 3.0f) * (rand()%3);
+		
+		DrawTexturePro(
+			crowd2,
+			source,
 			{
-				GetScreenWidth() - 2.0f * crowd2.width, 
-				GetScreenHeight() / 2 - crowd2.height	
+				GetScreenWidth() * 0.88, 
+				GetScreenHeight() / 3, 
+				2.0f * crowd.width, 
+				2.0f * crowd.height / 3.0f
 			}, 
-			0.0f, 
-			2.0f, 
+			{0.0f, 0.0f},
+			0.0f,
 			WHITE
 		);
 
